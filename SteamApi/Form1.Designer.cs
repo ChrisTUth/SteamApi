7dfa0deb0f61db4d928a17971ba2daf9cb7b0fc7
Chris Uth <ctuth@email.neit.edu>
2016-03-17 21:03:48 -0400

    Upload

---
+++ SteamApi/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace SteamApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.labNothing = new System.Windows.Forms.Label();
+            this.labPersonaName = new System.Windows.Forms.Label();
+            this.txtSteamID = new System.Windows.Forms.TextBox();
+            this.picAvatar = new System.Windows.Forms.PictureBox();
+            this.labSteamID = new System.Windows.Forms.Label();
+            this.btnAddRecord = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picAvatar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(185, 97);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // labNothing
+            // 
+            this.labNothing.AutoSize = true;
+            this.labNothing.Location = new System.Drawing.Point(6, 102);
+            this.labNothing.Name = "labNothing";
+            this.labNothing.Size = new System.Drawing.Size(52, 13);
+            this.labNothing.TabIndex = 1;
+            this.labNothing.Text = "Steam Id:";
+            // 
+            // labPersonaName
+            // 
+            this.labPersonaName.AutoSize = true;
+            this.labPersonaName.Location = new System.Drawing.Point(82, 12);
+            this.labPersonaName.Name = "labPersonaName";
+            this.labPersonaName.Size = new System.Drawing.Size(52, 13);
+            this.labPersonaName.TabIndex = 2;
+            this.labPersonaName.Text = "Waiting...";
+            // 
+            // txtSteamID
+            // 
+            this.txtSteamID.Location = new System.Drawing.Point(64, 99);
+            this.txtSteamID.Name = "txtSteamID";
+            this.txtSteamID.Size = new System.Drawing.Size(115, 20);
+            this.txtSteamID.TabIndex = 3;
+            // 
+            // picAvatar
+            // 
+            this.picAvatar.Location = new System.Drawing.Point(12, 12);
+            this.picAvatar.Name = "picAvatar";
+            this.picAvatar.Size = new System.Drawing.Size(64, 64);
+            this.picAvatar.TabIndex = 4;
+            this.picAvatar.TabStop = false;
+            // 
+            // labSteamID
+            // 
+            this.labSteamID.AutoSize = true;
+            this.labSteamID.Location = new System.Drawing.Point(82, 63);
+            this.labSteamID.Name = "labSteamID";
+            this.labSteamID.Size = new System.Drawing.Size(52, 13);
+            this.labSteamID.TabIndex = 5;
+            this.labSteamID.Text = "Waiting...";
+            // 
+            // btnAddRecord
+            // 
+            this.btnAddRecord.Location = new System.Drawing.Point(85, 29);
+            this.btnAddRecord.Name = "btnAddRecord";
+            this.btnAddRecord.Size = new System.Drawing.Size(112, 31);
+            this.btnAddRecord.TabIndex = 6;
+            this.btnAddRecord.Text = "Add User";
+            this.btnAddRecord.UseVisualStyleBackColor = true;
+            this.btnAddRecord.Click += new System.EventHandler(this.btnAddRecord_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 126);
+            this.Controls.Add(this.btnAddRecord);
+            this.Controls.Add(this.labSteamID);
+            this.Controls.Add(this.picAvatar);
+            this.Controls.Add(this.txtSteamID);
+            this.Controls.Add(this.labPersonaName);
+            this.Controls.Add(this.labNothing);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.Text = "SteamApi";
+            ((System.ComponentModel.ISupportInitialize)(this.picAvatar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label labNothing;
+        private System.Windows.Forms.Label labPersonaName;
+        private System.Windows.Forms.TextBox txtSteamID;
+        private System.Windows.Forms.PictureBox picAvatar;
+        private System.Windows.Forms.Label labSteamID;
+        private System.Windows.Forms.Button btnAddRecord;
+    }
+}
+
---
+++ SteamApi/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+namespace SteamApi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=57206F9C19CBFC1415091BFED660EB72&steamids=" + txtSteamID.Text;
+            Profile_ROOT steamUser = JsonConvert.DeserializeObject<Profile_ROOT>(new WebClient().DownloadString(url));
+            picAvatar.ImageLocation = steamUser.response.players[0].avatarmedium;
+            labPersonaName.Text = steamUser.response.players[0].personaname;
+            labSteamID.Text = steamUser.response.players[0].steamid;
+        }
+
+        private void btnAddRecord_Click(object sender, EventArgs e)
+        {
+            DataTable check = getDataTableFromStoredProcedureWithVars("stpFindUser", new string[1, 3] {
+                {"@steamid",labSteamID.Text,"varChar"}
+            });
+            if (check.Rows.Count == 0)
+            {
+                executeStoredProcedureWithVars("stpNewUser",new string[1, 3] {
+                    {"@steamid",labSteamID.Text,"varChar"}
+                });
+                //we refind it to get userid
+                check = getDataTableFromStoredProcedureWithVars("stpFindUser", new string[1, 3] {
+                    {"@steamid",labSteamID.Text,"varChar"}
+                });
+                string userId = check.Rows[0]["userId"].ToString();
+                //then we need to get games
+                string url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=57206F9C19CBFC1415091BFED660EB72&steamid="+check.Rows[0]["steamId"].ToString()+"&format=json";
+                G_ROOT playerGame = JsonConvert.DeserializeObject<G_ROOT>(new WebClient().DownloadString(url));
+                //then we need to add each game.
+                for (int uth = 0; uth < playerGame.response.game_count; uth++)
+                {
+                    executeStoredProcedureWithVars("stpNewGame",new string[3, 3] {
+                        {"@userId",userId,"int"},
+                        {"@appId",playerGame.response.games[uth].appid.ToString(),"int"},
+                        {"@playTime",playerGame.response.games[uth].playtime_forever.ToString(),"int"}
+                    });
+                }
+            }
+        }
+
+        public static DataTable getDataTableFromStoredProcedureWithVars(string stp, string[,] vars)
+        {
+            //ok to explain how to operate this beast.(i will be making more of them...then making them faster.
+            //stp is the name of the stored procedure
+            //the array MUST be a [?,3] does nt matter how many row just make sure you have 3 data per line
+            //[,0] is the name of the data the sql is expection: "@userId"
+            //[,1] is the value of the data you wanna send: 1
+            //[,2] is the type of data you are sending: varChar
+            //follow the rules and you do not have to type your own function.
+            DataTable dt = new DataTable();
+            SqlConnection cn = new SqlConnection("Data Source=localhost;Persist Security Info=True;User ID=steamapi;Password=test");
+            SqlCommand cmd = new SqlCommand(stp, cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            for (int loop1 = 0; loop1 < vars.GetLength(0); loop1++)
+            {
+                //list of supported types
+                //add one if you need it.
+                switch (vars[loop1, 2])
+                {
+                    case "varChar":
+                        cmd.Parameters.Add(vars[loop1, 0], SqlDbType.VarChar).Value = vars[loop1, 1];
+                        break;
+                    case "int":
+                        cmd.Parameters.Add(vars[loop1, 0], SqlDbType.Int).Value = Int32.Parse(vars[loop1, 1]);
+                        break;
+                    case "decimal":
+                        cmd.Parameters.Add(vars[loop1, 0], SqlDbType.Decimal).Value = Decimal.Parse(vars[loop1, 1]);
+                        break;
+                }
+            }
+            SqlDataAdapter data = new SqlDataAdapter(cmd);
+            try
+            {
+                cn.Open();
+                data.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                cn.Close();
+            }
+            try
+            {
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public static void executeStoredProcedureWithVars(string stp, string[,] vars)
+        {
+            //[,0] is the name of the data the sql is expection: "@userId"
+            //[,1] is the value of the data you wanna send: 1
+            //[,2] is the type of data you are sending: varChar
+            DataTable dt = new DataTable();
+            SqlConnection cn = new SqlConnection("Data Source=localhost;Persist Security Info=True;User ID=steamapi;Password=test");
+            SqlCommand cmd = new SqlCommand(stp, cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            for (int loop1 = 0; loop1 < vars.GetLength(0); loop1++)
+            {
+                //list of supported types
+                //add one if you need it.
+                switch (vars[loop1, 2])
+                {
+                    case "varChar":
+                        cmd.Parameters.Add(vars[loop1, 0], SqlDbType.VarChar).Value = vars[loop1, 1];
+                        break;
+                    case "int":
+                        cmd.Parameters.Add(vars[loop1, 0], SqlDbType.Int).Value = Int32.Parse(vars[loop1, 1]);
+                        break;
+                    case "decimal":
+                        cmd.Parameters.Add(vars[loop1, 0], SqlDbType.Decimal).Value = Decimal.Parse(vars[loop1, 1]);
+                        break;
+                    case "tinyInt":
+                        cmd.Parameters.Add(vars[loop1, 0], SqlDbType.TinyInt).Value = Boolean.Parse(vars[loop1, 1]);
+                        break;
+                }
+            }
+            SqlDataAdapter data = new SqlDataAdapter(cmd);
+            try
+            {
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    }
+    public class Player
+    {
+        public string steamid { get; set; }
+        public int communityvisibilitystate { get; set; }
+        public int profilestate { get; set; }
+        public string personaname { get; set; }
+        public int lastlogoff { get; set; }
+        public string profileurl { get; set; }
+        public string avatar { get; set; }
+        public string avatarmedium { get; set; }
+        public string avatarfull { get; set; }
+        public int personastate { get; set; }
+        public string realname { get; set; }
+        public string primaryclanid { get; set; }
+        public int timecreated { get; set; }
+        public int personastateflags { get; set; }
+        public string loccountrycode { get; set; }
+        public string locstatecode { get; set; }
+        public int loccityid { get; set; }
+    }
+    public class theplayers
+    {
+        public List<Player> players { get; set; }
+    }
+    public class Profile_ROOT
+    {
+        public theplayers response { get; set; }
+    }
+
+    public class G_game
+    {
+        public int appid { get; set; }
+        public int playtime_forever { get; set; }
+    }
+    public class G_games
+    {
+        public int game_count { get; set; }
+        public List<G_game> games { get; set; }
+    }
+    public class G_ROOT
+    {
+        public G_games response { get; set; }
+    }
+}
